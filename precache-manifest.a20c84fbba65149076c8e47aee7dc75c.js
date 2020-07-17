52cb41735df98d1b1eba079bc155018df8440c8c
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-07-17 23:21:53 +0300

    website release

---
+++ precache-manifest.a20c84fbba65149076c8e47aee7dc75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24a90b5c4c884ec1271a113f2809a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef48185fd321873f38ad",
+    "url": "/static/css/main.09143e69.chunk.css"
+  },
+  {
+    "revision": "67b5cb32e6921fd09675",
+    "url": "/static/js/2.af4a491a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af4a491a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef48185fd321873f38ad",
+    "url": "/static/js/main.76efc280.chunk.js"
+  },
+  {
+    "revision": "9f08f0e23da8310728e7",
+    "url": "/static/js/runtime-main.5b849a39.js"
+  }
+]);
